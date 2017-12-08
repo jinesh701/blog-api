44226896b17a68ae50cfd81b8c1d8ed64da14102
Jinesh Patel <jinesh701@gmail.com>
2017-12-07 23:57:18 -0500

    completed challenge

---
+++ blogPostsRouter.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const router = express.Router();
+
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+
+const { BlogPosts } = require("./models");
+
+BlogPosts.create(
+  "article title 1",
+  "This is the first test for content within a blog post",
+  "author 1"
+);
+BlogPosts.create(
+  "article title 2",
+  "This is the second test for content within a blog post",
+  "author 2"
+);
+
+//GET
+router.get("/", (req, res) => {
+  res.json(BlogPosts.get());
+});
+
+//POST
+router.post("/", jsonParser, (req, res) => {
+  const requiredFields = ["title", "content", "author"];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = BlogPosts.create(
+    req.body.title,
+    req.body.content,
+    req.body.author
+  );
+  return res.status(200).json(item);
+});
+
+//DELETE
+router.delete("/:id", (req, res) => {
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted blog post \`${req.params.id}\``);
+  res.status(204).end();
+});
+
+//UPDATE
+router.put("/:id", jsonParser, (req, res) => {
+  const requiredFields = ["id", "title", "content", "author", "publishDate"];
+  for (i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.log(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id != req.params.id) {
+    const message = `Request path id (${
+      req.params.id
+    }) and request body id ``(${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog posts \`${req.params.id}\``);
+  const updatedItem = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
+  });
+  res.status(204).end();
+});
+
+module.exports = router;
